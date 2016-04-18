382de3875ad4cbc61488809ee360f79830e3e9be
Drew Showalter <maelstroswim@gmail.com>
2016-04-19 07:52:27 +1000

    Switches request structure.

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 var express = require('express');
-var sfx = require("sfx");
+var request = require('request');
+var sfx = require('sfx');
 
 var app = express();
 var token = process.env.TOKEN;
 var key = process.env.KEY;
+console.log('key',key, 'token',token)
 
-require("jsdom").env("", function(err, window) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  var $ = require("jquery")(window);
-
-  $.get("https://api.trello.com/1/tokens/" + token
-    + "/webhooks/?key="+ key).then(function(ret) {
-    if (ret.length === 0) {
-      $.post("https://api.trello.com/1/tokens/" + token
-        + "/webhooks/?key=" + key,
+request
+  .get("https://api.trello.com/1/tokens/" + token
+    + "/webhooks/?key="+ key, function (error, res, body) {
+      if (error) console.log("Error1: ", error);
+      if (JSON.parse(body).length === 0) {
+      request
+        .post(
         {
-          description: "Listen for cards webhook.",
-          callbackURL: "http://www.dcding.heroku.com/card",
-          idModel: "4d5ea62fd76aa1136000000c",
-        }
-      );
-    }
+          url: "https://api.trello.com/1/webhooks?key=" + key + "&token=" + token,
+          formData: {
+              description: "Listen for cards webhook.",
+              callbackURL: "http://www.dcding.heroku.com/card",
+              idModel: "56aad7ebfe2e61b85876235f"
+          },
+          headers: {
+            'Content-Type': "application/x-www-form-urlencoded"
+          }
+        },
+        function(error, res, body) {
+          if (error) console.log("Error2: ", error);
+          console.log("Yay response: ", res);
+        });
+      }
   });
+
+app.post('/card', function (req, res) {
+  sfx.play("Submarine");
+  res.send(200)
 });
 
-app.post('/cards', function (req, res) {
-  sfx.play("Submarine");
+app.get('/', function (req, res) {
   res.send(200)
 });
 
