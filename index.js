c720a0f09023570023aefc3218c40101b9104ca9
Drew Showalter <maelstroswim@gmail.com>
2016-04-20 13:06:08 +1000

    Stringifies req.params for logging.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ request
   });
 
 app.post('/card', function (req, res) {
-  publisherClient.publish( 'updates', ('"' + req.params + '" card made') );
+  publisherClient.publish( 'updates', ('"' + JSON.stringify(req.params) + '" card made') );
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('All clients have received "' + req.params + '"');
   res.end();
