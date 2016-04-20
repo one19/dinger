4dc1c7cb3f6e728cd825d1bc6e05ffa5014b390b
Drew Showalter <maelstroswim@gmail.com>
2016-04-20 11:48:08 +1000

    Fix for redis issues.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var token = process.env.TOKEN;
 var key = process.env.KEY;
 
 var app = express();
-var publisherClient = redis.createClient(redisURL);
+var publisherClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+publisherClient.auth(redisURL.auth.split(":")[1]);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
