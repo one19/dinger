7c4ec54842b0eb6a10f46b40441339e8c811a4ea
Drew Showalter <maelstroswim@gmail.com>
2016-04-20 11:28:15 +1000

    Adds redis for storing SSE connections, and SSE connections that fire off sounds, and jade... Imean Pug rendering.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var request = require('request');
 var sfx = require('sfx');
+var redis = require('redis');
 
-var app = express();
+var port = process.env.PORT || 3000;
+var redisURL = process.env.REDIS_URL;
 var token = process.env.TOKEN;
 var key = process.env.KEY;
+
+var app = express();
+var publisherClient = redis.createClient(redisURL);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
 
 request
   .get("https://api.trello.com/1/tokens/" + token
@@ -33,16 +42,56 @@ request
   });
 
 app.post('/card', function (req, res) {
-  console.log(req, res)
-  sfx.play("Submarine");
-  res.send(200, JSON.stringify([req, res]))
+  publisherClient.publish( 'updates', ('"' + req.params + '" card made') );
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('All clients have received "' + req.params + '"');
+  res.end();
+});
+
+app.get('/update-stream', function(req, res) {
+  // let request last as long as possible
+  req.socket.setTimeout(0x7FFFFFFF);
+
+  var messageCount = 0;
+  var subscriber = redis.createClient();
+
+  subscriber.subscribe("updates");
+
+  // In case we encounter an error...print it out to the console
+  subscriber.on("error", function(err) {
+    console.log("Redis Error: " + err);
+  });
+
+  // When we receive a message from the redis connection
+  subscriber.on("message", function(channel, message) {
+    messageCount++; // Increment our message count
+
+    res.write('id: ' + messageCount + '\n');
+    res.write("data: " + message + '\n\n'); // Note the extra newline
+  });
+
+  //send headers for event-stream connection
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  res.write('\n');
+
+  // The 'close' event is fired when a user closes their browser window.
+  // In that situation we want to make sure our redis channel subscription
+  // is properly shut down to prevent memory leaks...and incorrect subscriber
+  // counts to the channel.
+  req.on("close", function() {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  });
 });
 
 app.get(['/', '/card'], function (req, res) {
-  res.send(200)
+  res.render('index');
 });
 
-var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Server running at http://localhost:' + port)
 });
